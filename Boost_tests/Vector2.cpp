206d78262c0a16b6b83e8a6ec02d5ba51746afb5
Yann Paillard <yann.paillard@laposte.net>
2023-10-12 15:28:35 +0200

    add test logic to project

---
+++ Boost_tests/Vector2.cpp
@@ -0,0 +1,17 @@
+//
+// Created by yann paillard on 12/10/2023.
+//
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN  // in only one cpp file
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(TwoTwoFour_suite)
+BOOST_AUTO_TEST_CASE(testPlus) {
+        BOOST_CHECK_EQUAL(2+2, 4);
+}
+BOOST_AUTO_TEST_CASE(testMult) {
+        BOOST_CHECK_EQUAL(2*2, 3);
+}
+BOOST_AUTO_TEST_SUITE_END()
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Gamepp
 game library on c++
+
+
+## Run Test :
+```bash
+mkdir build
+cd ./build
+cmake ../Boost_tests/CMakeLists.txt
+make
+./Boost_tests
+```
+
+## Run Example :
+```bash
+mkdir build
+cd ./build
+cmake ../CMakeLists.txt
+make
+./Gamepp
+```
+
